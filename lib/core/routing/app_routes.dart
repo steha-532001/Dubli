aa0a88cf487871daa2aba224f8eee3809db688c1
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-05-05 04:32:24 +0300

    Update app_routes.dart with new imports and route configurations
    Import necessary files and update route configurations in app_routes.dart to
    include new screens and cubits. Remove unused routes and commented-out code.

--- lib/core/routing/app_routes.dart
+++ lib/core/routing/app_routes.dart
@@ -1,4 +1,8 @@
-
+import 'package:dubli/core/routing/routes.dart';
+import 'package:dubli/core/utils/app_colors.dart';
+import 'package:dubli/feature/login/ui/views/login_screen.dart';
+import 'package:dubli/feature/splash/logic/cubit/splash_cubit.dart';
+import 'package:dubli/feature/splash/ui/views/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,73 +10,85 @@ class AppRoutes {
   static Route<dynamic>? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case Routes.initialRoute:
-        if (onBording != null) {
-          if (usertoken != null) {
-            return MaterialPageRoute(
-              builder: (context) => BlocProvider(
-                create: (context) => OnboardingCubit(),
-                child: const HomeView(),
-              ),
-            );
-          } else {
-            return MaterialPageRoute(
-              builder: (context) => BlocProvider(
-                create: (context) => LoginCubit(),
-                child: const LoginView(),
-              ),
-            );
-          }
-        } else {
-          return MaterialPageRoute(
-            builder: (context) => BlocProvider(
-              create: (context) => OnboardingCubit(),
-              child: const OnBordingView(),
-            ),
-          );
-        }
+        return MaterialPageRoute(
+          builder: (context) => BlocProvider(
+            create: (context) => SplashCubit(),
+            child: const SplashScreen(),
+          ),
+        );
 
       case Routes.loginViewsRoute:
         return MaterialPageRoute(
-          builder: (context) => BlocProvider(
-            create: (context) => LoginCubit(),
-            child: const LoginView(),
-          ),
+          builder: (context) => const LoginScreen(),
         );
-      case Routes.signUpViewsRoute:
-        return MaterialPageRoute(
-          builder: (context) => BlocProvider(
-            create: (context) => SignUpCubit(),
-            child: const SignUpView(),
-          ),
-        );
+      // case Routes.initialRoute:
+      //   if (onBording != null) {
+      //     if (usertoken != null) {
+      //       return MaterialPageRoute(
+      //         builder: (context) => BlocProvider(
+      //           create: (context) => OnboardingCubit(),
+      //           child: const HomeView(),
+      //         ),
+      //       );
+      //     } else {
+      //       return MaterialPageRoute(
+      //         builder: (context) => BlocProvider(
+      //           create: (context) => LoginCubit(),
+      //           child: const LoginView(),
+      //         ),
+      //       );
+      //     }
+      //   } else {
+      //     return MaterialPageRoute(
+      //       builder: (context) => BlocProvider(
+      //         create: (context) => OnboardingCubit(),
+      //         child: const OnBordingView(),
+      //       ),
+      //     );
+      //   }
 
-      case Routes.homeViewsRoute:
-        return MaterialPageRoute(
-          builder: ((context) => BlocProvider(
-                create: (context) => HomeCubit()..getUserData(),
-                child: const HomeView(),
-              )),
-        );
+      // case Routes.loginViewsRoute:
+      //   return MaterialPageRoute(
+      //     builder: (context) => BlocProvider(
+      //       create: (context) => LoginCubit(),
+      //       child: const LoginView(),
+      //     ),
+      //   );
+      // case Routes.signUpViewsRoute:
+      //   return MaterialPageRoute(
+      //     builder: (context) => BlocProvider(
+      //       create: (context) => SignUpCubit(),
+      //       child: const SignUpView(),
+      //     ),
+      //   );
 
-      case Routes.informationViewsRoute:
-        return MaterialPageRoute(
-          builder: ((context) => BlocProvider(
-                create: (context) => InformationCubit()..getInformationData(),
-                child: const InformationView(),
-              )),
-        );
+      // case Routes.homeViewsRoute:
+      //   return MaterialPageRoute(
+      //     builder: ((context) => BlocProvider(
+      //           create: (context) => HomeCubit()..getUserData(),
+      //           child: const HomeView(),
+      //         )),
+      //   );
 
-      case Routes.chatViewsRoute:
-        return MaterialPageRoute(
-          builder: ((context) => const ChatView()),
-        );
-      case Routes.mechanicInfoViewsRoute:
-        return MaterialPageRoute(
-          builder: ((context) => BlocProvider(
-                create: (context) => MechanicCubit(),
-                child: const MechanicInfoView(),
-              )),
-        );
+      // case Routes.informationViewsRoute:
+      //   return MaterialPageRoute(
+      //     builder: ((context) => BlocProvider(
+      //           create: (context) => InformationCubit()..getInformationData(),
+      //           child: const InformationView(),
+      //         )),
+      //   );
+
+      // case Routes.chatViewsRoute:
+      //   return MaterialPageRoute(
+      //     builder: ((context) => const ChatView()),
+      //   );
+      // case Routes.mechanicInfoViewsRoute:
+      //   return MaterialPageRoute(
+      //     builder: ((context) => BlocProvider(
+      //           create: (context) => MechanicCubit(),
+      //           child: const MechanicInfoView(),
+      //         )),
+      // );
 
       default:
         return _unFoundRoute();
