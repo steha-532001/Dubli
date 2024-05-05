e955b9b0488543483ddaf53c75a797a8364051ac
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-05-05 06:15:51 +0300

    Update LoginViewBody UI layout and styles
    Import necessary dependencies and update the UI layout and styles of LoginViewBody
    to improve the overall appearance and user experience.

--- lib/feature/login/ui/widgets/login_view_body.dart
+++ lib/feature/login/ui/widgets/login_view_body.dart
@@ -1,14 +1,60 @@
+import 'package:dubli/core/utils/app_colors.dart';
+import 'package:dubli/core/utils/app_image_assets.dart';
+import 'package:dubli/core/utils/app_styles.dart';
+import 'package:dubli/core/widgets/app_bottom.dart';
+import 'package:dubli/feature/login/ui/widgets/login_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-
-      ],
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 70),
+              Image.asset(
+                ImagesAssetsManager.applogoImage,
+                height: 150,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'DUPLI',
+                  style: AppStyle.font70Whitesemibold,
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: ColorManager.darkGreyColor,
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: const Column(
+                  children: [
+                    SizedBox(height: 50),
+                    LoginForm(),
+                    SizedBox(height: 40),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 60),
+              CustomBottom(
+                bottomHeight: 60,
+                onPressed: () {},
+                bottomtext: 'Login',
+                textBottomStyle: AppStyle.font30Primarymedium,
+                backgroundColor: ColorManager.darkyellowColor,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
