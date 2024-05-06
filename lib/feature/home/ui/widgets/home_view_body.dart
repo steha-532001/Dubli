0bb698e7cefe02947dd02ba97ff4f890dd8e415d
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-05-06 09:20:57 +0300

    Update home view body layout and add new views to layout repository
    - Update the layout of the home view body to include a centered text widget and an empty row.
    - Add ChatView, TimerView, EventsView, and SettingsView to the list of widgets returned by the layout repository implementation.

--- lib/feature/home/ui/widgets/home_view_body.dart
+++ lib/feature/home/ui/widgets/home_view_body.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/app_styles.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Home View',
+          style: AppStyle.font18Whitemedium,
+        ),
+        const Row()
+      ],
     );
   }
 }
--- lib/feature/layout/data/repositories/layout_repo_impl.dart
+++ lib/feature/layout/data/repositories/layout_repo_impl.dart
@@ -1,9 +1,14 @@
+import 'package:dubli/feature/chat/ui/views/chat_view.dart';
 import 'package:dubli/feature/home/ui/views/home_view.dart';
 import 'package:dubli/feature/layout/data/models/change_index_params.dart';
 import 'package:dubli/feature/layout/data/repositories/layout_repo.dart';
 import 'package:dubli/feature/layout/logic/cubit/layout_cubit.dart';
+import 'package:dubli/feature/setting/ui/views/settiings_view.dart';
+import 'package:dubli/feature/timer/ui/views/timer_view.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+
+import '../../../events/ui/views/events_view.dart';
 
 class LayoutRepoImpl extends LayOutRepo {
   LayoutRepoImpl();
@@ -21,11 +26,11 @@ class LayoutRepoImpl extends LayOutRepo {
   @override
   List<Widget> getBody() {
     return const <Widget>[
+      ChatView(),
+      TimerView(),
       HomeView(),
-      HomeView(),
-      HomeView(),
-      HomeView(),
-      HomeView(),
+      EventsView(),
+      SettingsView(),
     ];
   }
 
