eb48fa42142b5ed5b4dff84d51acd9a22ea31fe8
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-05-06 23:56:41 +0300

    Remove unused SignUpRepo and SignUpRepoImpl classes
    The SignUpRepo and SignUpRepoImpl classes were unused and have been removed
    from the project to clean up unnecessary code.

--- lib/feature/signup/data/repositories/sign_up_repo_impl.dart
+++
@@ -1,3 +0,0 @@
-import 'package:dubli/feature/signup/data/repositories/sign_up_repo.dart';
-
-class SignUpRepoImpl extends SignUpRepo {}
--- lib/feature/signup/logic/cubit/sign_up_state.dart
+++ lib/feature/signup/logic/cubit/sign_up_state.dart
@@ -4,3 +4,27 @@ part of 'sign_up_cubit.dart';
 sealed class SignUpState {}
 
 final class SignUpInitial extends SignUpState {}
+
+class SignUpFailed extends SignUpState {
+  final String error;
+
+  SignUpFailed({required this.error});
+}
+
+class SignUpSuccess extends SignUpState {}
+
+class SignUpLoading extends SignUpState {}
+
+
+class SaveUserDataLoading extends SignUpState {}
+
+class SaveUserDataSuccess extends SignUpState {}
+
+
+class SaveUserDataFailed extends SignUpState 
+{
+  final String error;
+
+  SaveUserDataFailed({required this.error});
+
+}
